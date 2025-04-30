a3551db5403c4d09a2daf499ffe877a46e611830
Sascha Stieglitz <sascha.stieglitz@web.de>
2025-04-30 12:07:58 +0200

    update check.sh msg to opensearch

--- Test/E2E/install/check.sh
+++ Test/E2E/install/check.sh
@@ -5,7 +5,7 @@ MAXCOUNT=$(cat /proc/sys/vm/max_map_count)
 MIN=262144
 if [ "$MAXCOUNT" -lt "$MIN" ]
 then
-    echo "Warning: vm.max_map_count is not high enough for Elasticsearch."
+    echo "Warning: vm.max_map_count is not high enough for Opensearch."
     echo "Edit /etc/sysctl.conf, add the following line and reboot:"
     echo "vm.max_map_count=262144"
     exit 1
