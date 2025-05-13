0ceb57e5acdb6ac4be29fdc0e630ab20e5a8e2e9
Sascha Stieglitz <sascha.stieglitz@mapp.com>
2025-05-13 13:27:00 +0200

    fix 2e2 check for macos

--- Test/E2E/install/check.sh
+++ Test/E2E/install/check.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 SCRIPT=$(readlink -f $0)
 INSTALL=`dirname $SCRIPT`
-MAXCOUNT=$(cat /proc/sys/vm/max_map_count)
-MIN=262144
-if [ "$MAXCOUNT" -lt "$MIN" ]
-then
-    echo "Warning: vm.max_map_count is not high enough for Opensearch."
-    echo "Edit /etc/sysctl.conf, add the following line and reboot:"
-    echo "vm.max_map_count=262144"
-    exit 1
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    MAXCOUNT=$(cat /proc/sys/vm/max_map_count)
+    MIN=262144
+    if [ "$MAXCOUNT" -lt "$MIN" ]
+    then
+        echo "Warning: vm.max_map_count is not high enough for Elasticsearch."
+        echo "Edit /etc/sysctl.conf, add the following line and reboot:"
+        echo "vm.max_map_count=262144"
+        exit 1
+    fi
 fi
+
 if [ ! -f $INSTALL/../../../.env ]
 then
     echo "Warning: .env file does not exists in project directory - create it like .env.example, or set the environment variable COMPOSER_AUTH yourself - the credentials can be found here: https://commercedeveloper.adobe.com/account/keys"
