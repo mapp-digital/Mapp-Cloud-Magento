0a8bd0ec184f2551e1fa6d98e72c5f1dd575a01b
asim <asim.mukhtar@ayko.com>
2026-05-27 12:16:31 +0100

    MPSLA-81: Updating Tracking version

--- Block/TrackingScript.php
+++ Block/TrackingScript.php
@@ -16,7 +16,7 @@ use Magento\Framework\View\Element\Template;
  */
 class TrackingScript extends Template
 {
-    public const PS_VERSION = "1.5.0";
+    public const PS_VERSION = "1.5.1";
 
     /**
      * @return bool
